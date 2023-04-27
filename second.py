9bd53482ae0baea6719b6b28bc52ef377b43d522
lanemunds <lanemunds@gmail.com>
2023-04-27 16:58:24 -0600

    second

--- second.py
+++ second.py
@@ -1,3 +1,14 @@
 import pandas
 data = pandas.read_csv('weather_data.csv')
-print(data)
+data_dict = data.to_dict()
+temp_list = data['temp'].to_list()
+total = 0
+for num in temp_list:
+    total += num
+avg_temp = total/len(temp_list)
+print(avg_temp)
+print(data['temp'].mean())
+
+print(data['temp'].max())
+
+print(data.condition)
