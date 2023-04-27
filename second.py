aba104c741d8eda4ac18c53e52dd0b72e786bd3c
lanemunds <lanemunds@gmail.com>
2023-04-27 17:31:36 -0600

    to csv

--- second.py
+++ second.py
@@ -6,9 +6,20 @@ total = 0
 for num in temp_list:
     total += num
 avg_temp = total/len(temp_list)
-print(avg_temp)
-print(data['temp'].mean())
+# print(avg_temp)
+# print(data['temp'].mean())
 
-print(data['temp'].max())
+# print(data['temp'].max())
 
-print(data.condition)
+# print(data.condition)
+
+# print(data[data.temp == data['temp'].max()])
+
+monday = data[data.day == 'Monday']
+
+data_dict_1 = {
+    'students': ['Amy', 'James', 'Angela'],
+    'scores': [76, 56, 65]
+}
+data_1 = pandas.DataFrame(data_dict_1)
+data_1.to_csv('new_data.csv')
